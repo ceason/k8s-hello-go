9bd95a09e4aa42cacd71129e43d113af1e48320b
Chris Eason <ceason@redventures.com>
2017-04-14 21:04:47 -0400

    basic hello world demo server

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var calls = 0
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	calls++
+	fmt.Fprintf(w, "Hello, world! You have called me %d times.\n", calls)
+}
+
+func main() {
+	http.HandleFunc("/", HelloWorld)
+	http.ListenAndServe(":8184", nil)
+	fmt.Printf("Started server at http://localhost%v.\n", ":8184")
+}
