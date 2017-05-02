cbfe12fa369b74d365fd326dd468cd69ae4a455e
Chris Eason <ceason@redventures.com>
2017-05-02 10:32:03 -0400

    triggering ci

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(time.Duration(rand.Int63n(50)) * time.Millisecond)
 
 	// "business logic" for service
-	fmt.Fprint(w, "Hello world")
+	fmt.Fprint(w, "Hello JD")
 }
 
 func main() {
